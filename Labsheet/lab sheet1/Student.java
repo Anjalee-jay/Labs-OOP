e5b3cde2b4f7d7fb370436e9a3f57555e7feec36
PGAI JAYATHISSA <116742462+Anjalee-jay@users.noreply.github.com>
2023-10-04 17:08:03 +0530

    Add files via upload

---
+++ Labsheet/lab sheet1/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+        String s = "Sam";
+        String j = "Jhon";
+
+        public static void main(String[] args) {
+
+            Student myObj1 = new Student();
+            Student myObj2 = new Student();
+        }
+            System.out.println(" FName:" + "\t" + myObj1.s);
+            System.out.println(" LName:" + "\t" + myObj2.j);
+        
+
+    } 
+    
+
